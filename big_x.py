c5797b07f60c4135c5e55adfc5dac946345d095a
Nina <g.nina@seznam.cz>
2021-03-07 20:36:29 +0100

    Inserted big_x.py

--- big_x.py
+++ big_x.py
@@ -1,1 +1,12 @@
 # Write a function that draws a capital letter X of size (n) from the characters '#'.
+def big_x(n):
+    for i in range(n):
+        for j in range(n):
+            if i + j == n - 1 or i - j == 0:
+                print('#', end='')
+            else:
+                print(' ', end='')
+        print()
+
+
+big_x(7)
