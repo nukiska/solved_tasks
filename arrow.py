7148d1162b05a3d24c6448c34d759e8509234a1b
Nina <g.nina@seznam.cz>
2021-03-08 15:40:48 +0100

    Inserted maximum_neighbors.py

---
+++ arrow.py
@@ -0,0 +1,14 @@
+#  Write a function that draws a text image of an arrow from the characters '*'
+#  inside a square of size (n) formed by dots. You can assume that (n) is odd.
+
+def arrow(n):
+    for i in range(n):
+        for j in range(n):
+            c = '.'
+            if i == n // 2 or j - i == n // 2 or i + j == n // 2 + (n - 1):
+                c = '*'
+            print(c, end='')
+        print()
+
+
+arrow(7)  # odd number
